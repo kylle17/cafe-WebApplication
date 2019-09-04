a68731b614db2eda8b2d604a2d88d84171768c00
parkjongwoong <whddnd0211@naver.com>
2019-09-04 21:30:04 +0900

    돌아감

--- src/main/java/com/admin/order/dao/AdminOrderDao.java
+++ src/main/java/com/admin/order/dao/AdminOrderDao.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.admin.order.domain.AdminOrderListVO;
-import com.member.domain.MemberVO;
 
 @Repository
 public class AdminOrderDao {
