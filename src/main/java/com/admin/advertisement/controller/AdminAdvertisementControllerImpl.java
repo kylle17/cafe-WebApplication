79768404d1f531b60ca0ca7ec708f68cbe684b88
parkjongwoong <whddnd0211@naver.com>
2019-09-09 09:57:43 +0900

    no message

--- src/main/java/com/admin/advertisement/controller/AdminAdvertisementControllerImpl.java
+++ src/main/java/com/admin/advertisement/controller/AdminAdvertisementControllerImpl.java
@@ -1,70 +1,70 @@
-package com.admin.advertisement.controller;
-
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.advertisement.domain.AdvertisementVO;
-import com.advertisement.service.AdvertisementServiceImpl;
-
-@Controller
-@RequestMapping(value="/adminAd")
-public class AdminAdvertisementControllerImpl implements AdminAdvertisementController{
-
-	@Autowired
-	private AdvertisementServiceImpl advertisementService;
-
-	
-	@RequestMapping(value="/main")
-	public ModelAndView adminAdMain(@RequestParam("sideMenu") String sideMenu ,HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mov = new ModelAndView();
-		
-		String viewName = "adminAd/main";
-		mov.setViewName(viewName);
-		
-		HttpSession session = request.getSession();		
-		session.setAttribute("sideMenu", sideMenu );
-		
-		
-		List<AdvertisementVO> advertisementList = advertisementService.advertisementList();
-		mov.addObject("advertisementList", advertisementList);
-		
-		return mov;
-	}
-	
-	
-	@RequestMapping(value="/update")
-	public ModelAndView adminAdUpdate(@RequestParam("sideMenu") String sideMenu ,HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mov = new ModelAndView();
-		
-		String viewName = "adminAd/update";
-		mov.setViewName(viewName);
-		
-		HttpSession session = request.getSession();		
-		session.setAttribute("sideMenu", sideMenu );
-		
-		
+//package com.admin.advertisement.controller;
+//
+//import java.util.List;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.dao.DataAccessException;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import com.advertisement.domain.AdvertisementVO;
+//import com.advertisement.service.AdvertisementServiceImpl;
+//
+//@Controller
+//@RequestMapping(value="/adminAd")
+//public class AdminAdvertisementControllerImpl implements AdminAdvertisementController{
+//
+//	@Autowired
+//	private AdvertisementServiceImpl advertisementService;
+//
+//	
+//	@RequestMapping(value="/main")
+//	public ModelAndView adminAdMain(@RequestParam("sideMenu") String sideMenu ,HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		ModelAndView mov = new ModelAndView();
+//		
+//		String viewName = "adminAd/main";
+//		mov.setViewName(viewName);
+//		
+//		HttpSession session = request.getSession();		
+//		session.setAttribute("sideMenu", sideMenu );
+//		
+//		
 //		List<AdvertisementVO> advertisementList = advertisementService.advertisementList();
 //		mov.addObject("advertisementList", advertisementList);
-		
-		return mov;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+//		
+//		return mov;
+//	}
+//	
+//	
+//	@RequestMapping(value="/update")
+//	public ModelAndView adminAdUpdate(@RequestParam("sideMenu") String sideMenu ,HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		ModelAndView mov = new ModelAndView();
+//		
+//		String viewName = "adminAd/update";
+//		mov.setViewName(viewName);
+//		
+//		HttpSession session = request.getSession();		
+//		session.setAttribute("sideMenu", sideMenu );
+//		
+//		
+////		List<AdvertisementVO> advertisementList = advertisementService.advertisementList();
+////		mov.addObject("advertisementList", advertisementList);
+//		
+//		return mov;
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//}
